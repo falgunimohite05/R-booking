7cb8f771d66d68719c2fb6241232669136412d7a
Falguni Mohite <65585037+falgunimohite05@users.noreply.github.com>
2022-04-18 21:57:19 +0530

    Update settings.py

--- rail/settings.py
+++ rail/settings.py
@@ -32,6 +32,7 @@ CSRF_TRUSTED_ORIGINS = ['https://r-booking.azurewebsites.net', 'https://rbooking
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'home.apps.HomeConfig',
     'features.apps.FeaturesConfig',
     'django.contrib.admin',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,6 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
